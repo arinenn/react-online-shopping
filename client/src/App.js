4ecd507936d96b988c07055b2c878a5f588c005d
arinenn <arinenn@ya.ru>
2023-01-12 12:41:45 +0300

    skeleton created, router is done

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,12 @@
+import { BrowserRouter } from 'react-router-dom';
+import AppRouter from './components/AppRouter';
+
 const App = () => {
-  return <div className="App">WORKING</div>;
+  return (
+    <BrowserRouter>
+      <AppRouter />
+    </BrowserRouter>
+  );
 };
 
 export default App;
---
+++ client/src/routes.js
@@ -0,0 +1,43 @@
+import Admin from './pages/AdminPage';
+import Auth from './pages/AuthPage';
+import Basket from './pages/BasketPage';
+import DevicePage from './pages/DevicePage';
+import Shop from './pages/ShopPage';
+import {
+  ADMIN_ROUTE,
+  BASKET_ROUTE,
+  DEVICE_ROUTE,
+  LOGIN_ROUTE,
+  REGISTRATION_ROUTE,
+  SHOP_ROUTE,
+} from './utilities/consts';
+
+export const authRoutes = [
+  {
+    path: ADMIN_ROUTE,
+    Component: Admin,
+  },
+  {
+    path: BASKET_ROUTE,
+    Component: Basket,
+  },
+];
+
+export const publicRoutes = [
+  {
+    path: SHOP_ROUTE,
+    Component: Shop,
+  },
+  {
+    path: LOGIN_ROUTE,
+    Component: Auth,
+  },
+  {
+    path: REGISTRATION_ROUTE,
+    Component: Auth,
+  },
+  {
+    path: DEVICE_ROUTE + '/:id',
+    Component: DevicePage,
+  },
+];
